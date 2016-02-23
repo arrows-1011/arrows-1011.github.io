f5a47fb60d9264324e9f8150881e220235b78fdd
arrows-1011 <s1210207@gmail.com>
2016-02-23 20:40:14 +0900

    [Fix] GPA Calculator

--- School/gpa.js
+++ School/gpa.js
@@ -17,5 +17,5 @@ function calc() {
     var C = Number(document.getElementById('C').value);
     var F = Number(document.getElementById('F').value);
     var grade = new Grade(A, B, C, F);
-    document.getElementById('result').innerHTML = grade.calcGPA();
+    document.getElementById('result').innerHTML = "Your GPA is " + grade.calcGPA();
 }
